5f7e2b72d8c70c7d15c38a400f8049446c066c26
Jonathan Elsner <jelsnerbusiness@outlook.com>
2021-03-18 23:59:27 -0700

    Move SpacetimeObjectList to own file

--- spacetime_diagram/GUI.java
+++ spacetime_diagram/GUI.java
@@ -5,12 +5,10 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.CardLayout;
-import java.util.AbstractSequentialList;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Hashtable;
 import java.util.List;
-import java.util.ListIterator;
 import java.util.stream.Collectors;
 
 import javax.swing.BorderFactory;
@@ -27,7 +25,6 @@ import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.JScrollPane;
 import javax.swing.JSlider;
-import javax.swing.ListModel;
 import javax.swing.event.ListDataEvent;
 import javax.swing.event.ListDataListener;
 
@@ -279,92 +276,5 @@ public class GUI extends JFrame {
         public Object getSelectedItem() {
             return selectedItem;
         }
-    }
-
-    private class SpacetimeObjectList extends AbstractSequentialList<SpacetimeObject>
-            implements ListModel<SpacetimeObject> {
-
-        private ArrayList<SpacetimeObject> objects;
-        private ArrayList<ListDataListener> listeners;
-
-        public SpacetimeObjectList() {
-            objects = new ArrayList<>();
-            listeners = new ArrayList<>();
-        }
-
-        @Override
-        public int getSize() {
-            return objects.size();
-        }
-
-        @Override
-        public SpacetimeObject getElementAt(int index) {
-            return objects.get(index);
-        }
-
-        @Override
-        public void addListDataListener(ListDataListener l) {
-            listeners.add(l);
-        }
-
-        @Override
-        public void removeListDataListener(ListDataListener l) {
-            listeners.remove(l);
-        }
-
-        @Override
-        public boolean add(SpacetimeObject obj) {
-            if (objects.add(obj)) {
-                ListDataEvent e = new ListDataEvent(this, ListDataEvent.INTERVAL_ADDED, objects.size() - 2,
-                        objects.size() - 1);
-                listeners.forEach(l -> l.intervalAdded(e));
-
-                return true;
-            } else {
-                return false;
-            }
-        }
-
-        @Override
-        public boolean remove(Object obj) {
-            int i = objects.indexOf(obj);
-
-            if (objects.remove(obj)) {
-                ListDataEvent e = new ListDataEvent(this, ListDataEvent.INTERVAL_REMOVED, i, i);
-                listeners.forEach(l -> l.intervalRemoved(e));
-
-                return true;
-            } else {
-                return false;
-            }
-        }
-
-        @Override
-        public SpacetimeObject remove(int i) {
-            SpacetimeObject o = objects.remove(i);
-
-            if (o != null) {
-                ListDataEvent e = new ListDataEvent(this, ListDataEvent.INTERVAL_REMOVED, i, i);
-                listeners.forEach(l -> l.intervalRemoved(e));
-            }
-
-            return o;
-        }
-
-        @Override
-        public SpacetimeObject get(int index) {
-            return objects.get(index);
-        }
-
-        @Override
-        public int size() {
-            return objects.size();
-        }
-
-        @Override
-        public ListIterator<SpacetimeObject> listIterator(int index) {
-            return objects.listIterator(index);
-        }
-
     }
 }
---
+++ spacetime_diagram/SpacetimeObjectList.java
@@ -0,0 +1,97 @@
+package spacetime_diagram;
+
+import java.util.AbstractSequentialList;
+import java.util.ArrayList;
+import java.util.ListIterator;
+
+import javax.swing.ListModel;
+import javax.swing.event.ListDataEvent;
+import javax.swing.event.ListDataListener;
+
+import spacetime_diagram.LorentzTransform.SpacetimeObject;
+
+class SpacetimeObjectList extends AbstractSequentialList<SpacetimeObject> implements ListModel<SpacetimeObject> {
+
+    private ArrayList<SpacetimeObject> objects;
+    private ArrayList<ListDataListener> listeners;
+
+    public SpacetimeObjectList() {
+        objects = new ArrayList<>();
+        listeners = new ArrayList<>();
+    }
+
+    @Override
+    public int getSize() {
+        return objects.size();
+    }
+
+    @Override
+    public SpacetimeObject getElementAt(int index) {
+        return objects.get(index);
+    }
+
+    @Override
+    public void addListDataListener(ListDataListener l) {
+        listeners.add(l);
+    }
+
+    @Override
+    public void removeListDataListener(ListDataListener l) {
+        listeners.remove(l);
+    }
+
+    @Override
+    public boolean add(SpacetimeObject obj) {
+        if (objects.add(obj)) {
+            ListDataEvent e = new ListDataEvent(this, ListDataEvent.INTERVAL_ADDED, objects.size() - 2,
+                    objects.size() - 1);
+            listeners.forEach(l -> l.intervalAdded(e));
+
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public boolean remove(Object obj) {
+        int i = objects.indexOf(obj);
+
+        if (objects.remove(obj)) {
+            ListDataEvent e = new ListDataEvent(this, ListDataEvent.INTERVAL_REMOVED, i, i);
+            listeners.forEach(l -> l.intervalRemoved(e));
+
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public SpacetimeObject remove(int i) {
+        SpacetimeObject o = objects.remove(i);
+
+        if (o != null) {
+            ListDataEvent e = new ListDataEvent(this, ListDataEvent.INTERVAL_REMOVED, i, i);
+            listeners.forEach(l -> l.intervalRemoved(e));
+        }
+
+        return o;
+    }
+
+    @Override
+    public SpacetimeObject get(int index) {
+        return objects.get(index);
+    }
+
+    @Override
+    public int size() {
+        return objects.size();
+    }
+
+    @Override
+    public ListIterator<SpacetimeObject> listIterator(int index) {
+        return objects.listIterator(index);
+    }
+
+}
